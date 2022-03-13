8db3a1541019071a8874046acdfc796f5ab22326
stefzouille <contact.lokcy@gmail.com>
2022-03-13 17:24:12 +0100

    add fetch api et innerhtml

--- front/js/script.js
+++ front/js/script.js
@@ -1,47 +1,54 @@
 // // 1 - afficher les produits sur la page d accueil
 
-const items = document.getElementById('items');
-const Url = 'http://localhost:3000/api/products';
-
-let api = fetch('http://localhost:3000/api/products')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-    return data;
-  });
-console.log("c est juste la" + api);
 
 
-// let data = [];
-// for (const elem of data) {
-//   array.push(elem) = api;
+
+// const promiseGetProduct = new Promise((resolve, reject) => {
+//   if (PRODUCT !== 'undefine') {
+//     resolve(PRODUCT);
+//   } else {
+//     reject('Accés aux produits impossible !');
+//   }
+// })
+
+const url = 'http://localhost:3000/api/products';
+
+document.querySelector("#nom-de l id de la page html")
+
+// recuperer les données de l api
+fetch(url)
+  .then(response => response.json()
+    .then(data => {
+      console.log(data);
+      let affichage = '<ol>';
+      for (let product of data) {
+        //recup des paires clé valeurs de l api
+        affichage += `<li> nom : ${product.name}  - description : ${product.description}</li>`;
+      }
+      affichage += '</ul>';
+      // affiche sur la page html avec l id de la balise div
+      document.querySelector('#items').innerHTML = affichage;
+    })
+  ).catch(err => console.log("Erreur : " + err));
+
+
+
+
+
+// class canape {
+//   constructor(colors, _id, name, price, imageUrl, description, altTxt) {
+//     this.colors = colors;
+//     this.id = _id;
+//     this.name = name;
+//     this.price = price;
+//     this.imageUrl = imageUrl;
+//     this.description = description;
+//     this.altTxt = altTxt;
+//   }
 // }
-// console.log(api);
 
 
-class canape {
-  constructor(colors, _id, name, price, imageUrl, description, altTxt) {
-    this.colors = colors;
-    this.id = _id;
-    this.name = name;
-    this.price = price;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.altTxt = altTxt;
-  }
-}
 
-
-// //chercher dans l api les produits
-// let canap1 = new canape(api.colors, api._id, api.name, api.price, api.imageUrl, api.description, api.altTxt);
-// let canap = new canape(1, "canape", "20", "img/canape.jpg", "canape", "10");
-// let canap2 = new canape(2, "canape2", "20", "img/canape2.jpg", "canape2", "10");
-// let canap3 = new canape(3, "canape3", "20", "img/canape3.jpg", "canape3", "10");
-// let canap4 = new canape(4, "canape4", "20", "img/canape4.jpg", "canape4", "10");
-
-// console.log(canap1);
 
 
 
