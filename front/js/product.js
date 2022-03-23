5107557620d87a18a332847e04dfd5308e373354
stefzouille <contact.lokcy@gmail.com>
2022-03-23 19:28:00 +0100

    add colors js product

--- front/js/product.js
+++ front/js/product.js
@@ -1,57 +1,75 @@
 // recup urlsearchparams page produit
+const url = 'http://localhost:3000/api/products/';
 
-console.log(window.location);
-// nouvelle objet de type urlsearchparams et ds ce constructeur on recupere la chaine de charactere a manipuler
 var searchParams = new URLSearchParams(window.location.search);
-
-// search a des methode pour manip les characteres
-// methode has pour verifier ds le params si existe ds l url un params nommé id
-
-console.log(searchParams.has('id')); // return true 
 
 // si le params id existe ds l url 
 if (searchParams.has('id')) {
   // aller rechercher la valeur de l attribut id ds l url avec la methode get
-  var ProductId = searchParams.get('id');
-  console.log(ProductId);
+  var productId = searchParams.get('id');
 
   // si il existe pas renvoie vers la page d accueil
 } else {
   window.location.href = './index.html';
 }
+
+
 function produit() {
-  fetch(searchParams)
+  fetch(url + productId)
     .then(response => response.json()
       .then(data => {
-        console.log('data:', data);
+        // console.log('data:', data);
 
-        // var Produits = document.getElementById('items');
-        // console.log(Produits);
+        var objetProduits = document.querySelector('.item');
+
+        var product = data
+        // var objet = document.createElement('article');
+
+        console.log(product);
+
+        var images = document.createElement('img');
+        images.setAttribute('src', product.imageUrl);
+        images.setAttribute('alt', product.altTxt);
+        var contenairImg = document.querySelector('.item__img');
+        contenairImg.appendChild(images);
+
+        var title = document.createElement('h1');
+        // title.setAttribute('id', product.name);
+        title.textContent = product.name;
+        var contenairTitle = document.querySelector('#title');
+        contenairTitle.appendChild(title);
+
+        var price = document.createElement('span');
+        price.textContent = product.price;
+        var contenairPrice = document.querySelector('#price');
+        contenairPrice.appendChild(price);
+
+
+        var description = document.createElement('p');
+        description.textContent = product.description;
+        var contenairDescription = document.querySelector('#description');
+        contenairDescription.appendChild(description);
+
+        // selection de la couleur
+        var color = document.createElement('option');
+        color.textContent = product.color;
+        var contenairColor = document.querySelector('#color');
+        contenairColor.appendChild(color);
+
+        // contenairColor.appendChild(value);
+        console.log(contenairColor);
+
+
+
+
+
+
+        // console.log(title);
+        // console.log(objetProduits);
+
+
       })
     ).catch(error => alert("Erreur : " + error));
 }
 
-// produit();
-
-
-// recuperer les données de l api
-
-//         for (let product of data) {
-
-//           var objet = document.createElement('section');
-
-//           var img = document.createElement('img');
-//           img.setAttribute('src', product.imageUrl);
-//           img.setAttribute('alt', product.altTxt);
-//           img.textContent = product.imageUrl;
-//           // baliseProduits.appendChild(img);
-
-//           // baliseProduits.appendChild(img)
-//           console.log(img);
-//         }
-//       })
-//     ).catch(error => alert("Erreur : " + error));
-// }
-
-// produit();
-
+produit();
