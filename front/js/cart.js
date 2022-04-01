63491563baf1a689efaa73f8ea73f352c513b013
stefzouille <contact.lokcy@gmail.com>
2022-04-01 16:33:24 +0200

    ad

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,66 +1,53 @@
-// recuperer localstorage et api
-// ajouter un produit au panier
-// supprimer un produit du panier
-// modifier la quantité d'un produit du panier
-// afficher le panier
-// afficher le prix total du panier
-// afficher le nombre de produit dans le panier
+// parcourir local storage avec foreach et selectionner mon element puis sup ou rajouter...
 
-const url = 'http://localhost:3000/api/products/';
 // recuperer le tableau dans le local storage
 var addProduit = JSON.parse(localStorage.getItem('product'));
 // console.log(addProduit);
 
-console.log(url);
-console.log(addProduit);
 
-function produit() {
-  fetch(url + addProduit[1].id)
-    .then(response => response.json()
-      .then(data => {
+// verifier si il y a bien un produit dans le local storage
+const basketDisplay = () => {
+  console.log("salut");
+  if (addProduit) {
+    addProduit;
+    console.log(addProduit);
+    for (const element of addProduit) {
+      console.log(element);
+    }
+    // afficher le local storage dans le html
+    addProduit.forEach(function (product) {
+      var productId = product.id;
+      var productColor = product.color;
+      var productQuantity = product.quantity;
+      var productPrice = product.price;
 
-        var product = data
-        console.log(product);
-      })
-    ).catch(error => console.log("erreur : " + error));
-}
-produit();
-
-
-
-// const url = 'http://localhost:3000/api/products/';
-
-// // // recuperer le tableau dans le local storage
-// var addProduit = JSON.parse(localStorage.getItem('product'));
-// console.log(addProduit);
-// var apiStotrage = [];
+      var productPanier = document.getElementById("cart__items")
 
 
+      var panierId = document.createElement("div");
+      panierId.setAttribute("class", "panier__id");
+      panierId.textContent = product.id;
+      productPanier.appendChild(panierId);
 
-// function items() {
-//   fetch(url)
-//     .then(response => response.json()
-//       .then(data => {
-//         console.log('data:', data);
-//         apiStotrage = data;
-//         var afficher = document.getElementById('cart__items');
-//         console.log(apiStotrage);
+      var panierColor = document.createElement("div");
+      panierColor.textContent = product.color;
+      productPanier.appendChild(panierColor);
 
-//         var title = document.createElement('h1');
-//         title.textContent = apiStotrage.name;
-
-//         afficher.appendChild(title);
+      var panierQuantity = document.createElement("div");
+      panierQuantity.textContent = product.quantity;
+      productPanier.appendChild(panierQuantity);
 
 
-
-//         console.log(afficher);
-//       })
-//     ).catch(error => alert("Erreur : " + error));
-// }
-// items();
-// apiStotrage.push(addProduit);
+      console.log(productPanier);
+      var panierTotal = document.createElement("div");
+      panierTotal.textContent = product.total;
+      productPanier.appendChild(panierTotal);
+    })
 
 
-
-
-
+  } else {
+    addProduit = [];
+    console.log(addProduit + "vide");
+  };
+};
+basketDisplay();
