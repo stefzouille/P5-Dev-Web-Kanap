0a4b6687be2538723c722fc423b19c547c83f66b
stefzouille <contact.lokcy@gmail.com>
2022-04-05 16:20:11 +0200

    add dynamique storage & api

--- front/js/cart.js
+++ front/js/cart.js
@@ -10,7 +10,20 @@ var panier = JSON.parse(localStorage.getItem('product'));
 // console.log(panier);
 
 // foreach pour parcourir le tableau et faire un fetch a chaque element du tableau
-panier.forEach(function (product) {
+panier.forEach(function (product, index) {
+  product['index'] = index;
+  let _id = [];
+  let name = [];
+  let price = [];
+  let imageUrl = [];
+  let altTxt = [];
+  _id.push(product.id);
+  console.log(_id);
+
+  // comparer l id de l api avec l id du localstorage
+
+  // console.log(product);
+
   var productId = product.id;
   var productColor = product.color;
   var productQuantity = product.quantity;
@@ -18,46 +31,77 @@ panier.forEach(function (product) {
   // console.log(productColor);
   // console.log(productQuantity);
 
-  fetch(url)
+  fetch(url + productId)
     .then(response => response.json()
       .then(data => {
+
+        // product c est l api
         var product = data;
         console.log(product);
+        // --------------------------------------------recuperer les donnees de l api------------------------------------------------------
 
+        //recup le name de l api et push ds mon array vide declaré dans mon panier.foreach
+        name.push(product.name);
+        console.log(product.name);
+
+        //recup le prix de l api et push ds mon array vide declaré dans mon panier.foreach
+        price.push(product.price);
+        console.log(product.price);
+
+        //recup l image de l api et push ds mon array vide declaré dans mon panier.foreach
+        imageUrl.push(product.imageUrl);
+        console.log(product.imageUrl);
+
+        //recup l alt de l api et push ds mon array vide declaré dans mon panier.foreach
+        altTxt.push(product.altTxt);
+        console.log(product.altTxt);
+
+        // --------------------------------------------afficher sur le dom------------------------------------------------------
+
+        // prod recup info du dom
         var prod = document.getElementById('cart__items');
-        console.log(prod);
+        // console.log(prod);
 
+        // creation d un div cart__item
         var item = document.createElement('cart__item');
         item.classList.add('cart__item');
         prod.appendChild(item);
 
+        // afficher le nom ds le dom
+        var prodName = document.createElement('cart__item__name');
+        prodName.textContent = product.name;
+        item.appendChild(prodName);
 
-        var prodId = document.createElement('h3');
-        prodId.textContent = productId;
-        item.appendChild(prodId);
+        // afficher le prix ds le dom
+        var prodPrice = document.createElement('cart__item__price');
+        prodPrice.textContent = product.price;
+        item.appendChild(prodPrice);
 
-        var prodColor = document.createElement('p');
+        //afficher image ds le dom
+        var prodImg = document.createElement('cart__item__img');
+        prodImg.setAttribute('src', product.imageUrl);
+        prodImg.setAttribute('alt', product.altTxt);
+        prodImg.textContent = product.altTxt;
+        item.appendChild(prodImg);
+
+        // afficher l image ds le dom
+        var prodColor = document.createElement('cart__item__color');
         prodColor.textContent = productColor;
         item.appendChild(prodColor);
+
+
+        // creation titre h3
+        // var prodId = document.createElement('h3');
+        // prodId.textContent = productId;
+        // item.appendChild(prodId);
+
+        // var prodColor = document.createElement('p');
+        // prodColor.textContent = productColor;
+        // item.appendChild(prodColor);
 
         var prodQuantity = document.createElement('p');
         prodQuantity.textContent = productQuantity;
         item.appendChild(prodQuantity);
-
-        // integrate array in the cart
-        var cart = data.filter(function (product) {
-          return productId;
-        });
-        console.log(cart);
-
-
-
-
-
-
-
-
-
 
       })
     ).catch(error => console.log("erreur : " + error));
