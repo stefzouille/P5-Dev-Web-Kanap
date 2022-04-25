ec34ed1b57d321cb68e07f14be8989757bc24de7
stefzouille <contact.lokcy@gmail.com>
2022-04-25 17:43:00 +0200

    add reg - test

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -4,7 +4,7 @@ var searchParams = new URLSearchParams(window.location.search);
 
 // console.log(searchParams.getAll('orderId'));
 
-// afficher l orderId en remplacement du span
+// afficher l orderId en remplaçant le contenu du span id="orderId"
 var orderId = document.querySelector('#orderId');
 orderId.textContent = searchParams.getAll('orderId');
 
